87a8b9a506188be7bfd7809037d8561bc6ae1f06
viktorkode <69812060+viktorkode@users.noreply.github.com>
2020-09-14 18:36:55 +0200

    Project creation

---
+++ BookApp/BookApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookApp
+//
+//  Created by Viktor on 14/9/20.
+//  Copyright © 2020 Code Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
