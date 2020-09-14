72ba2c78a8eef05bb7b9c135dd31e07754f0c153
viktorkode <69812060+viktorkode@users.noreply.github.com>
2020-09-14 18:44:46 +0200

    Test comment

--- BookApp/BookApp/ViewController.swift
+++ BookApp/BookApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Test comment
         // Do any additional setup after loading the view.
     }
 
